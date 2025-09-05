b6491b025163976cc52171d8c1a56b415f6a0ac4
vincenttommi <vincenttommikorir@gmail.com>
2025-09-05 21:05:12 +0300

    setting up enviroment

--- belissimo_back/users/models.py
+++ belissimo_back/users/models.py
@@ -12,3 +12,46 @@ from drf_spectacular.utils import extend_schema_field
 
 
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email_address,password=None, **extra_fields):
+        if not email_address:
+            raise ValueError("The Email must be set")
+        
+
+        email_address = self.normalize_email(email_address)
+        user = self.model(email_address=email_address, **extra_fields)
+
+
+        user.save(using=self.__db)
+        return user
+    
+
+    def create_superuser(self,email_address,pasword=None, **extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        extra_fields.setdefault("provider","email")
+        extra_fields.setdefault("verified_at",timezone.now())
+
+
+        if not pasword:
+            raise ValueError("Superuser must have a password")
+        
+
+        return self.create_user(email_address, password, **extra_fields)
+    
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+  class Role(models.TextChoices):
+    ADMIN = "admin","Admin"
+    LANDLORD = "landlord","Landlord"
+    TENANT = "tenat","Tenant"
+
+
+
+
+    id  = models.UUIDField(primary_key=True,default=uuid4, editable=False)
+    role = models.CharField(max_length=20,choices=Role.choices, default=Role.TENANT)
+    first
